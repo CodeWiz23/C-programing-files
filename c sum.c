8bf21e87da39d0f80938b9723c259c94911c1cf1
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-15 21:48:34 +0600

    Add files via upload

---
+++ c sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+
+    int a, b, sum ;
+
+    printf ("Enter your numbers :");
+
+    scanf ("%d %d", &a, &b);
+
+    sum = a + b ;
+
+    printf ("The sum is :%d",sum);
+
+    return 0;
+
+}
