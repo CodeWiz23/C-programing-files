8f3cfbabc075eca9441f98e0f3f60684dd794f6b
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-16 21:43:10 +0600

    Add files via upload

---
+++ c division.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    float a, b, div;
+
+    printf ("Enter your numbers :");
+
+    scanf("%f %f", &a,  &b );
+
+
+    div = a / b;
+
+
+    printf ("Your division is : %.2f",div);
+
+
+
+
+
+
+}
