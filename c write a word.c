8ed53954e593c0e9fe7d49e8d7368699df490ec7
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-19 22:08:30 +0600

    Add files via upload

---
+++ c write a word.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+
+    char word [20];
+
+    printf("Enter your word : ");
+
+    scanf ("%s", &word);
+
+    printf ("your word is : %s",word);
+
+    return 0;
+
+}
